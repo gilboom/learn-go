c45290c10fbaa89a3cafe80af11d295359fb6301
linxiji <linxiji@tuzhanai.com>
2021-03-21 22:25:02 +0800

    update

--- reflection/walk.go
+++ reflection/walk.go
@@ -3,8 +3,61 @@ package reflection
 import "reflect"
 
 func Walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+	//if val.Kind() == reflect.Slice {
+	//	for i := 0; i < val.Len(); i++ {
+	//		Walk(val.Index(i).Interface(), fn)
+	//	}
+	//	return
+	//}
+	//
+	//for i := 0; i < val.NumField(); i++ {
+	//	field := val.Field(i)
+	//
+	//	//if field.Kind() == reflect.String {
+	//	//	fn(field.String())
+	//	//}
+	//	//
+	//	//if field.Kind() == reflect.Struct {
+	//	//	Walk(field.Interface(), fn)
+	//	//}
+	//
+	//	switch field.Kind() {
+	//	case reflect.String:
+	//		fn(field.String())
+	//	case reflect.Struct:
+	//		Walk(field.Interface(), fn)
+	//	}
+	//}
+
+	walkValue := func(v reflect.Value) {
+		Walk(v.Interface(), fn)
+	}
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, k := range val.MapKeys() {
+			walkValue(val.MapIndex(k))
+		}
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
 	val := reflect.ValueOf(x)
-	field := val.Field(0)
-	field.Type()
-	fn(field.String())
+
+	if val.Kind() == reflect.Ptr {
+		return val.Elem()
+	}
+
+	return val
 }
--- reflection/walk_test.go
+++ reflection/walk_test.go
@@ -1,25 +1,139 @@
 package reflection
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestWalk(t *testing.T) {
 
-	expected := "Chris"
-	var got []string
-
-	x := struct {
-		Name string
-	}{expected}
-
-	Walk(x, func(input string) {
-		got = append(got, input)
-	})
-
-	if len(got) != 1 {
-		t.Errorf("wrong number of function calls, got %d but want %d", len(got), 1)
+	type Profile struct {
+		Age  int
+		City string
 	}
 
-	if got[0] != expected {
-		t.Errorf("got %q but want %q", got[0], expected)
+	type Person struct {
+		Name    string
+		Profile Profile
+	}
+
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		expectedCalls []string
+	}{
+		{
+			"Struct with one string field",
+			struct{ Name string }{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Struct with non string field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 25},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with nested struct field",
+			Person{
+				"Chris",
+				Profile{25, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Pointers to things",
+			&Person{
+				"Chris",
+				Profile{25, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Slices",
+			[]Profile{
+				{33, "London"},
+				{25, "Shanghai"},
+			},
+			[]string{"London", "Shanghai"},
+		},
+		{
+			"Arrays",
+			[2]Profile{
+				{33, "London"},
+				{34, "Shanghai"},
+			},
+			[]string{"London", "Shanghai"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			Walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.expectedCalls) {
+				t.Errorf("got %v but want %v", got, test.expectedCalls)
+			}
+		})
+	}
+
+	t.Run("Maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		Walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+
+	//expected := "Chris"
+	//var got []string
+	//
+	//x := struct {
+	//	Name string
+	//}{expected}
+	//
+	//Walk(x, func(input string) {
+	//	got = append(got, input)
+	//})
+	//
+	//if len(got) != 1 {
+	//	t.Errorf("wrong number of function calls, got %d but want %d", len(got), 1)
+	//}
+	//
+	//if got[0] != expected {
+	//	t.Errorf("got %q but want %q", got[0], expected)
+	//}
+}
+
+func assertContains(t *testing.T, haystack []string, needle string) {
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+
+	if !contains {
+		t.Errorf("expected %+v to contain %q but it didn't", haystack, needle)
 	}
 }
